6a06a174358ed5630388b9e286b6d1b9bdda4f53
Roberto Frías Bernal <roberfrias115@gmail.com>
2016-12-30 17:52:18 +0100

    Cambio carpeta bin por compilacion

---
+++ compilacion/CMakeFiles/SistemasDistribuidos.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/frias/Documents/SistemasDistribuidos/Arbol.cpp" "/home/frias/Documents/SistemasDistribuidos/bin/CMakeFiles/SistemasDistribuidos.dir/Arbol.cpp.o"
+  "/home/frias/Documents/SistemasDistribuidos/Disco.cpp" "/home/frias/Documents/SistemasDistribuidos/bin/CMakeFiles/SistemasDistribuidos.dir/Disco.cpp.o"
+  "/home/frias/Documents/SistemasDistribuidos/Nodo.cpp" "/home/frias/Documents/SistemasDistribuidos/bin/CMakeFiles/SistemasDistribuidos.dir/Nodo.cpp.o"
+  "/home/frias/Documents/SistemasDistribuidos/Terminal.cpp" "/home/frias/Documents/SistemasDistribuidos/bin/CMakeFiles/SistemasDistribuidos.dir/Terminal.cpp.o"
+  "/home/frias/Documents/SistemasDistribuidos/main.cpp" "/home/frias/Documents/SistemasDistribuidos/bin/CMakeFiles/SistemasDistribuidos.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ compilacion/CMakeFiles/SistemasDistribuidos.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/SistemasDistribuidos.dir/Arbol.cpp.o"
+  "CMakeFiles/SistemasDistribuidos.dir/Disco.cpp.o"
+  "CMakeFiles/SistemasDistribuidos.dir/main.cpp.o"
+  "CMakeFiles/SistemasDistribuidos.dir/Nodo.cpp.o"
+  "CMakeFiles/SistemasDistribuidos.dir/Terminal.cpp.o"
+  "SistemasDistribuidos.pdb"
+  "SistemasDistribuidos"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/SistemasDistribuidos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
